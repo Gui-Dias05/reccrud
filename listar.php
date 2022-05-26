f29b4b12e1db2c528bae11326929e48a112af241
Gui-Dias05 <diasarielguilherme@gmail.com>
2022-05-26 18:09:28 -0300

    att trab

--- listar.php
+++ listar.php
@@ -17,7 +17,7 @@
     }
 
     $procurar = isset($_POST["procurar"]) ? $_POST["procurar"] : ""; 
-    $buscar = isset($_POST['buscar']) ? $_POST['buscar'] : 1;
+    $buscar = isset($_POST['buscar']) ? $_POST['buscar'] : 0;
 ?>
 <html>
 <head>
@@ -38,71 +38,43 @@
         <input type="radio" name="buscar" value="1" <?php if ($buscar == "1") echo "checked" ?>> Id<br>
         <input type="radio" name="buscar" value="2" <?php if ($buscar == "2") echo "checked" ?>> lado<br>
         <input type="radio" name="buscar" value="3" <?php if ($buscar == "3") echo "checked" ?>> Cor<br>
-    <h3>Procurar Quadrado:</h3>
-        <input type="text" name="procurar" id="procurar" size="25" value="<?php echo $procurar;?>">
-            <br><br>
-        <button name="acao" id="acao" type="submit">Procurar </button>
-            <br><br>
-</form>
-
-<?php
- 
-    ?>
-
-<div>
-        <table>
-            <thead>
-                <tr>
-                    <th scope="col"> | #ID | </th>
-                    <th scope="col"> | Lado | </th>
-                    <th scope="col"> | Cor | </th>
-                    <th scope="col"> | Mostrar quadrado | </th>
-                    <th scope="col"> | Editar | </th>
-                    <th scope="col"> | Excluir | </th>
-
-                </tr>
-            </thead>
-            <tbody>
-            <?php
-    
-    $pdo = Conexao::getInstance(); 
-    if($buscar == 1){
-        $buscar = $pdo->query("SELECT * FROM quadrado
-                                 WHERE id LIKE '$procurar%' 
-                                 ORDER BY id");}
-
-    else if($buscar == 2){
-        $buscar = $pdo->query("SELECT * FROM quadrado
-                                 WHERE lado LIKE '$procurar%' 
-                                 ORDER BY lado");}
-
-    else if($buscar == 3){
-        $buscar = $pdo->query("SELECT * FROM quadrado
-                                WHERE cor LIKE '$procurar%' 
-                                ORDER BY id");}
-    while ($linha = $buscar->fetch(PDO::FETCH_ASSOC)) { 
-
-        $cor = str_replace("#","%23",$linha['cor']);
-
-        ?>
-                <tr>
-                    <th scope="row"><?php echo $linha['id'];?></th>
-                    <th scope="row"><?php echo $linha['lado'];?></th>
-                    <td scope="row"><?php echo $linha['cor'];?></td>
-                    <td scope="row"><a href="mostrar.php?lado=<?php echo $linha['lado'];?>&cor=<?php echo str_replace('#', '%23',  $linha['cor']);?>">Quadrado</a></td> 
-                    <td><a href='index.php?acao=editar&id=<?php echo $linha['id'];?>'> <img src="img/editar.png" style="width: 1.8vw;"></a></td>
-                    <td><?php echo " <a href=javascript:excluirRegistro('processa.php?acao=excluir&id={$linha['id']}')>";?><img src="img/excluir.png" style="width: 1.5vw;"></a></td>
-                    
-                </tr>
-            <?php } ?> 
-            
-            </tbody>
-        </table>
-    </div>
+            <h3>Procurar Quadrado:</h3>
+        <input type="text" name="procurar" id="procurar"  value="<?php echo $procurar;?>">
+                <br><br>
+        <button name="acao" id="acao" type="submit">Procurar</button>
+        <br><br>
+    </form>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">#ID</th>
+                        <th scope="col">Lado</th>
+                        <th scope="col">Cor</th>
+                        <th scope="col">Mostrar</th>
+                        <th scope="col">Alterar</th>
+                        <th scope="col">Deletar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    $quad = new quadrado("","", "");
+                    $lista = $quad->listar($buscar, $procurar);
+                    foreach ($lista as $linha) { 
+                ?>
+                    <tr>
+                        <th scope="row"><?php echo $linha['id'];?></th>
+                        <th scope="row"><?php echo $linha['lado'];?></th>
+                        <td scope="row"><?php echo $linha['cor'];?></td>
+                        <td scope="row"><a href="mostrar.php?id=<?php echo $linha['id']; ?>&lado=<?php echo $linha['lado'];?>&cor=<?php echo str_replace('#', '%23', $linha['cor']);?>">Quadrado</a></td>
+                        <td scope="row"><a href="index.php?acao=editar&id=<?php echo $linha['id'];?>">Alterar</a></td>
+                        <td><?php echo " <a href=javascript:excluirRegistro('processa.php?acao=excluir&id={$linha['id']}')>Excluir</a><br>";?></td>
+                    </tr>
+                <?php } ?> 
+                </tbody>
+            </table>
+        </div>
     <button><a href='index.php'>Voltar</a> </button>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
-    
 </body>
 </html>
